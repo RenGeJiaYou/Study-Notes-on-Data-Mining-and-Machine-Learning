3851a717150a33f03a761510917c7d5d903c5a26
Shi Jiajun <1178972961@qq.com>
2022-10-17 22:04:58 +0800

    10.17 初步开始 k-NN 算法

---
+++ 1.k-NN/kNN.py
@@ -0,0 +1,22 @@
+from numpy import *  # ’import numpy‘也可以，不过之后调用就要写numpy.array(...)
+import operator
+
+
+# 创建数据集和标签
+def createDataSet():
+    group = array([[1, 101], [5, 89], [108, 5], [115, 8]])  # 四组二维特征
+    labels = ['爱情片', '爱情片', '动作片', '动作片']  # 四组特征的标签
+    return group, labels
+
+
+# k-NN 分类算法
+def classify0(inX,dataSet,labels,k):
+
+
+if __name__ == '__main__':
+    # 创建数据集
+    group, labels = createDataSet()
+
+    # 打印数据集
+    print(group)
+    print(labels)
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Study-Notes-on-Data-Mining-and-Machine-Learning
-《数据挖掘和机器学习》仓库，记录学习心得与相关源码😊
+《数据挖掘和机器学习》仓库，记录学习心得与相关源码😊  
+
+## 概述
+![img.png](img.png)
+
+## 一.k-NN Algorithm
