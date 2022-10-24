c761910abbcbbcc713f4731fc02c78500b113aa5
Shi Jiajun <1178972961@qq.com>
2022-10-24 18:18:46 +0800

    10.24 决策树算法
    1.了解经验熵：每一个分类已经得知的数据集，都可以拥有一个经验熵

--- 1.k-NN/kNN.py
+++ 1.k-NN/kNN.py
@@ -1,8 +1,14 @@
 from numpy import *  # ’import numpy‘也可以，不过之后调用就要写numpy.array(...)
 import operator
 
-
-# 创建数据集和标签
+"""
+ Describe: 创建数据集和标签
+ Params:
+    无
+ Returns:
+    group:  数据集
+    labels: 标签集
+"""
 def createDataSet():
     group = array([[1, 101], [5, 89], [108, 5], [115, 8]])  # 四组二维特征
     labels = ['爱情片', '爱情片', '动作片', '动作片']  # 四组特征的标签
